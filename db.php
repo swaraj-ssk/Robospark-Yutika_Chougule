f56b4e15c8883af5d7442ebe17eb566c62df3446
Swaraj Kalbande <67674718+swaraj-ssk@users.noreply.github.com>
2020-08-04 10:01:10 +0530

    Delete db.php

--- db.php
+++
@@ -1,9 +0,0 @@
-<?php
-$con=mysqli_connect('localhost','root',"",'studid');
-if(!$con)
-{
-    echo "connection error".mysqli_connect_error();
-}
-else 
-   echo "<h3> Database connection success....</h3>";
-?>
